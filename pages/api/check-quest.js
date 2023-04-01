73ec1f9f88602e8a22d0ff0c396a6fc249322b83
Tomcroms <thomaslaumonier@orange.fr>
2023-04-01 11:54:34 +0200

    env variable

--- pages/api/check-quest.js
+++ pages/api/check-quest.js
@@ -1,17 +1,21 @@
 import mysql from 'mysql2/promise';
 
 export default async function handler (req, res) {
+    console.log("Connecting to database");
     const dbconnection = await mysql.createConnection({
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
     });
+    console.log("Connected to database");
     try{
+        console.log("Executing query");
         const query = "SELECT challengeSpeedyOpenEdition from table1 where wallet = ?";
         const { walletAddress } = req.body;
         const values = [walletAddress];
         const [data] = await dbconnection.execute(query, values);
+        console.log("Query executed, data:", data);
         dbconnection.end();
         if (data.length > 0 && data[0].challengeSpeedyOpenEdition === 5) {
             res.status(200).json({ authorized: true });
